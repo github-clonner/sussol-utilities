3ce620751d41e435364899d017c5e4006d00acb3
Edwin <edwin@sussol.net>
2017-02-07 17:35:28 +1300

    Update comments on formatters

--- src/formatters.js
+++ src/formatters.js
@@ -1,3 +1,9 @@
+/**
+* Returns the date object passed in as a string in either the format dd/mm/yyyy or dd.mm.yyyy
+* @param  {Date}   date    The date to format and return as a string
+* @param  {string} format  Either 'slashes', 'dots', or undefined (will use slashes)
+* @return {string}         The formatted string
+*/
 export function formatDate(date, format) {
   if (!date || typeof date !== 'object') return null;
   switch (format) {
@@ -13,11 +19,24 @@ function getMonth(date) {
   return date.getMonth() + 1; // Date.getMonth is 0 indexed
 }
 
+/**
+* Returns the date object passed in as a formatted string showing the date and time
+* @param  {Date}   date    The date (including time) to format and return as a string
+* @param  {string} format  Either 'slashes', 'dots', or undefined (will use slashes)
+* @return {string}         The formatted string
+*/
 export function formatDateAndTime(date, format) {
   const time = date.toLocaleTimeString();
   return `${formatDate(date, format)} ${time.substring(0, time.length - 3)}`;
 }
 
+/**
+* Returns the string chopped off at maxLength, with ... replacing the last three characters if it
+* overflowed
+* @param  {string}   string     The string to be truncated
+* @param  {integer}  maxLength  The maximum number of characters in the returned string
+* @return {string}              The truncated string, with ellipses if it overflowed
+*/
 export function truncateString(string, maxLength) {
   if (string.length <= maxLength) return string;
   return `${string.substring(0, maxLength - 3)}...`;
