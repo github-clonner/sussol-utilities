ef38c70075bed410a620b791d876607f6df9042e
Edwin <edwin@sussol.net>
2017-02-08 11:24:40 +1300

    Add comments to Scheduler and parsers

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -2,16 +2,29 @@
  * Sustainable Solutions (NZ) Ltd. 2016
  */
 
+/**
+* A Scheduler provides the ability to schedule functions after an interval, and maintains handles on
+* any scheduled functions so they can be canceled
+*/
 export class Scheduler {
   constructor() {
     this.intervalIds = [];
   }
 
+  /**
+  * Schedules the callback function to be executed after interval milliseconds
+  * @param {function} onInterval The function to be executed after a delay
+  * @param {integer}  interval   The number of milliseconds before the function should be called
+  */
   schedule(onInterval, interval) {
     const intervalId = setInterval(onInterval, interval);
     this.intervalIds = [...this.intervalIds, intervalId];
   }
 
+  /**
+  * Cancels any scheduled functions that have not been executed. Should be called for safety before
+  * the Scheduler instance goes out of scope
+  */
   clearAll() {
     for (const intervalId of this.intervalIds) {
       clearInterval(intervalId);
--- src/parsers.js
+++ src/parsers.js
@@ -1,3 +1,8 @@
+/**
+* Returns the integer represented by numberString, or 0 if it is negative or not a number
+* @param  {string}  numberString  The string to be parsed, should represent a number
+* @return {integer}               The positive integer represented by numberString, or 0
+*/
 export function parsePositiveInteger(numberString) {
   if (!numberString || numberString.length < 1) return null;
   const number = parseFloat(numberString);
