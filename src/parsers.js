7c8ffa178c802b3f6acc3d3c86980d386fbbd0c9
Andrei <andrei@andreis-MacBook-Pro.local>
2017-04-05 16:32:33 +1200

    Add parsePositiveFloat
    Could make common function for check on the first line of both parsers

--- src/parsers.js
+++ src/parsers.js
@@ -9,3 +9,14 @@ export function parsePositiveInteger(numberString) {
   if (isNaN(number)) return 0; // Invalid strings become 0
   return Math.max(Math.round(number), 0); // Negative numbers become 0
 }
+/**
+* Returns the float represented by numberString, or 0 if it is negative or not a number
+* @param  {string}  numberString  The string to be parsed, should represent a number
+* @return {integer}               The positive float represented by numberString, or 0
+*/
+export function parsePositiveFloat(numberString) {
+  if (!numberString || numberString.length < 1) return null;
+  const number = parseFloat(numberString);
+  if (isNaN(number) || number < 0) return 0; // Invalid strings or negatives become 0
+  return number; 
+}
